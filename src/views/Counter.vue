e175ceeb977fc9e9f99f6534e09dc6641474da5c
edson-alexandre <edson.ass@gmail.com>
2022-04-10 11:01:20 -0300

    Incluíndo título no componente

---
+++ src/views/Counter.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    <h1>Contador</h1>
+    <div>
+      <button v-on:click="decrement">-</button>
+      {{ formatedCounter }}
+      <button v-on:click="increment">+</button>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+import Component from "vue-class-component";
+
+@Component
+export default class Counter extends Vue {
+  count = 0;
+
+  //Isso é uma variável computada
+  get formatedCounter(): string {
+    return `Soma: ${this.count}`;
+  }
+
+  increment(): void {
+    this.count++;
+  }
+
+  decrement(): void {
+    this.count--;
+  }
+
+  mounted(): void {
+    console.log("Counter");
+  }
+}
+</script>
+
+<style></style>
